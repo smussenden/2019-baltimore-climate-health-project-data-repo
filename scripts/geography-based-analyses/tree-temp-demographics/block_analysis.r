2248e6d247713ea3cc7e7e5c1d7ac0a8c7cbba0e
smussenden <smussenden@gmail.com>
2019-06-25 17:20:11 -0400

    update

--- scripts/geography-based-analyses/tree-temp-demographics/block_analysis.r
+++ scripts/geography-based-analyses/tree-temp-demographics/block_analysis.r
@@ -1,4 +1,4 @@
-#### Analysis of ZIP Code Tabulation Areas
+#### Analysis of Blocks 
 #### 
 
 #################################################################
@@ -15,18 +15,21 @@ library(magrittr)
 ######## Load Data Produced in Cleaning.r Script File ###########
 #################################################################
 
-block_tree_temp_demographics <- read_csv("output/data/cleaned/blocks_tree_temp_demographics.csv")  
-block_tree_temp_demographics <- block_tree_temp_demographics %<>% 
-  mutate(GEOID10 = as.character(GEOID10)) %>%
-  rename(block_id = GEOID10)
-  
+block_tree_temp_demographics <- read_csv("data/output-data/cleaned/tree-temp-demographic-w-naip-lidar-use/blocks_lidartree_temp_demographics.csv")
+
+block_tree_temp_demographics <- block_tree_temp_demographics %>% 
+  mutate(geoid10 = as.character(geoid10)) %>%
+  rename(block_id = geoid10)
+
 
 #################################################################
 ########## Define Functions #####################################
 #################################################################
 
 # Run functions.R
-source("scripts/analysis/functions.R")
+source("scripts/geography-based-analyses/tree-temp-demographics/functions.R")
+
+glimpse(block_tree_temp_demographics)
 
 # Select computable values within *this particular* df
 select_x <- function(df){
@@ -58,6 +61,79 @@ cleanup <- function() {
 ##################################
 ### Heat vs demographics #########
 ##################################
+# Not useful based on demographic information we have.  Valid block level data is VERY old, 2010.
+
+
+
+##################################
+### Trees vs demographics ########
+##################################
+# Not useful based on demographic information we have.  Valid block level data is VERY old, 2010.
+
+##################################
+### Trees vs heat ################
+##################################
+        
+# Build correlation matrix
+tree_vs_heat_block_correlation_matrix <- block_tree_temp_demographics %>%
+  select_x() %>%
+  as.matrix() %>%
+  correlate() %>%
+  focus(matches("63"), matches("127"), matches ("lid")) %>%
+  mutate(variable=rowname) %>%
+  filter(str_detect(variable, "^temp_")) %>%
+  select(variable, everything(), -rowname) 
+
+# Write it out to a csv for later use
+write_matrix_csv(tree_vs_heat_block_correlation_matrix)
+
+# Make correlation long instead of wide so it can be passed to ggplot correctly. 
+tree_vs_heat_block_correlation_matrix_long <- tree_vs_heat_block_correlation_matrix %>%
+  gather("variable_2", "value", 2:13) %>%
+  arrange(desc(value))
+
+# Build graphic
+make_correlation_matrix_graphic(tree_vs_heat_block_correlation_matrix_long)
+
+# Remove all but master file and functions
+cleanup()
+
+##################################
+### Tree cover vs tree change ####
+##################################
+
+# Build correlation matrix
+treecover_vs_coverchange_block_correlation_matrix <- block_tree_temp_demographics %>%
+  select_x() %>%
+  as.matrix() %>%
+  correlate() %>%
+  focus(-matches("change"), -matches("temp"), -matches("population")) %>%
+  mutate(variable=rowname) %>%
+  filter(str_detect(variable, "change")) %>%
+  select(variable, everything(), -rowname) 
+
+# Write it out to a csv for later use
+write_matrix_csv(treecover_vs_coverchange_block_correlation_matrix)
+
+# Make correlation long instead of wide so it can be passed to ggplot correctly. 
+treecover_vs_coverchange_block_correlation_matrix_long <- treecover_vs_coverchange_block_correlation_matrix %>%
+  gather("variable_2", "value", 2:7) %>%
+  arrange(desc(value))
+
+# Build graphic
+make_correlation_matrix_graphic(treecover_vs_coverchange_block_correlation_matrix_long)
+
+# Remove all but master file and functions
+cleanup()
+
+
+
+
+
+
+
+
+
 # Build correlation matrix
 heat_vs_demographics_block_correlation_matrix <- block_tree_temp_demographics %>%
   select_x() %>%
@@ -112,87 +188,3 @@ cleanup()
 # new <- rcorr(block_tree_temp_demographics$temp_mean_am, block_tree_temp_demographics$temp_median_am, type="pearson")
 # rcorr(block_tree_temp_demographics$temp_mean_aft, block_tree_temp_demographics$walk_score)
 # baltzips <- c(21201,21202,21205,21206,21207,21208,21209,21210,21211,21212,21213,21214,21215,21216,21217,21218,21222,21223,21224,21225,21226,21227,21228,21229,21230,21231,21234,21236,21237,21239,21251)
-
-
-##################################
-### Trees vs demographics ########
-##################################
-# Build correlation matrix
-tree_vs_demographics_block_correlation_matrix <- block_tree_temp_demographics %>%
-  select_x() %>%
-  as.matrix() %>%
-  correlate() %>%
-  focus(matches("1718"), matches("09"), matches("change_percent")) %>%
-  mutate(variable=rowname) %>%
-  select(variable, everything(), -rowname)
-
-# Write it out to a csv for later use
-write_matrix_csv(tree_vs_demographics_block_correlation_matrix)
-
-# Make correlation long instead of wide so it can be passed to ggplot correctly. 
-tree_vs_demographics_block_correlation_matrix_long <- tree_vs_demographics_block_correlation_matrix %>%
-  gather("variable_2", "value", 2:9) %>%
-  arrange(desc(value))
-
-# Build graphic
-make_correlation_matrix_graphic(tree_vs_demographics_block_correlation_matrix_long)
-
-# Remove all but master file and functions
-cleanup()
-
-
-##################################
-### Trees vs heat ################
-##################################
-        
-# Build correlation matrix
-tree_vs_heat_block_correlation_matrix <- block_tree_temp_demographics %>%
-  select_x() %>%
-  as.matrix() %>%
-  correlate() %>%
-  focus(matches("1718"), matches("17"), matches("09"), matches("change_percent")) %>%
-  mutate(variable=rowname) %>%
-  filter(str_detect(variable, "^temp_")) %>%
-  select(variable, everything(), -rowname) 
-
-# Write it out to a csv for later use
-write_matrix_csv(tree_vs_heat_block_correlation_matrix)
-
-# Make correlation long instead of wide so it can be passed to ggplot correctly. 
-tree_vs_heat_block_correlation_matrix_long <- tree_vs_heat_block_correlation_matrix %>%
-  gather("variable_2", "value", 2:9) %>%
-  arrange(desc(value))
-
-# Build graphic
-make_correlation_matrix_graphic(tree_vs_heat_block_correlation_matrix_long)
-
-# Remove all but master file and functions
-cleanup()
-
-##################################
-### Tree cover vs tree change ####
-##################################
-
-# Build correlation matrix
-treecover_vs_coverchange_block_correlation_matrix <- block_tree_temp_demographics %>%
-  select_x() %>%
-  as.matrix() %>%
-  correlate() %>%
-  focus(matches("1718"), matches("17"), matches ("09")) %>%
-  mutate(variable=rowname) %>%
-  filter(str_detect(variable, "change_percent")) %>%
-  select(variable, everything(), -rowname) 
-
-# Write it out to a csv for later use
-write_matrix_csv(treecover_vs_coverchange_block_correlation_matrix)
-
-# Make correlation long instead of wide so it can be passed to ggplot correctly. 
-treecover_vs_coverchange_block_correlation_matrix_long <- treecover_vs_coverchange_block_correlation_matrix %>%
-  gather("variable_2", "value", 2:5) %>%
-  arrange(desc(value))
-
-# Build graphic
-make_correlation_matrix_graphic(treecover_vs_coverchange_block_correlation_matrix_long)
-
-# Remove all but master file and functions
-cleanup()
