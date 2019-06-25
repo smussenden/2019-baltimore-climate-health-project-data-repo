0aa3873f1b7d3cfb5d7e7b31e7fb4f40d30b5ec6
smussenden <smussenden@gmail.com>
2019-06-25 16:35:39 -0400

    update

--- scripts/geography-based-analyses/tree-temp-demographics/nsa_analysis.r
+++ scripts/geography-based-analyses/tree-temp-demographics/nsa_analysis.r
@@ -107,7 +107,7 @@ write_matrix_csv(treecover_vs_coverchange_nsa_correlation_matrix)
 
 # Make correlation long instead of wide so it can be passed to ggplot correctly. 
 treecover_vs_coverchange_nsa_correlation_matrix_long <- treecover_vs_coverchange_nsa_correlation_matrix %>%
-  gather("variable_2", "value", 2:5) %>%
+  gather("variable_2", "value", 2:7) %>%
   arrange(desc(value))
 
 # Build graphic
