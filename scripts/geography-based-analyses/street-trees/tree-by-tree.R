6b115ef943dab1a7839644a4bdebb1f0fc05499b
Roxanne Ready <shardsofblue@gmail.com>
2019-06-26 11:25:23 -0400

    updated street tree analysis

--- scripts/geography-based-analyses/street-trees/tree-by-tree.R
+++ scripts/geography-based-analyses/street-trees/tree-by-tree.R
@@ -23,40 +23,62 @@ rm(list=ls())
 # setwd("/Users/rready/Desktop/baltimore-climate-project")
 
 #####################
-### Begin Analysis
+##### Load data #####
 #####################
 
-condition_levels <- factor(c("dead",
-                             "poor",
-                             "fair",
-                             "good",
-                             "absent",
-                             "sprouts",
-                             "stump",
-                             "stump w",
-                             "n/a"),
-                           ordered = TRUE
-                           )
-
-
-tree_by_tree <- read_csv("../baltimore-climate-project/data/treecover/street_trees_nsa_join_table.csv") %>%
+tree_by_tree <- read_csv("data/treecover/street_trees_nsa_join_table.csv") %>%
   select(-COLOR_2, -LABEL) %>%
   rename_all(tolower) %>%
   mutate_all(tolower) %>%
-  mutate_at(vars(matches("street"), matches("onstr"), 
-                 matches("side"), matches("spp"),
-                 matches("condition"), matches("mt"),
-                 matches("loc_type"), matches("space_type"),
-                 matches("utilities"), matches("hard_scape"),
-                 matches("cultivar"), matches("common"),
-                 matches("genus"), matches("nbrdesc")
-                 ), 
+  mutate(condition = ifelse(condition == "stump w", "stump", condition)) %>%
+  # Make "condition" a factor so it can be ordered
+  mutate_at(vars(matches("condition")), 
             as.factor) %>%
+  mutate(condition = fct_relevel(condition, 
+                                 c("absent",
+                                   "stump",
+                                   "dead",
+                                   "poor",
+                                   "fair",
+                                   "good",
+                                   "sprouts",
+                                   "n/a")
+  )
+  ) %>%
   mutate_at(vars(matches("tree_ht"), matches("dbh")), as.numeric)
-  #mutate_at(vars(matches("nbrdesc")), tolower)
+#mutate_at(vars(matches("nbrdesc")), tolower)
 
 
-tree_condition_by_nbr <- tree_by_tree %>% 
+######################
+### Begin analysis ###
+######################
+
+## Examine cagegories of information
+
+tree_by_tree %>% 
+  group_by(condition) %>%
+  dplyr::summarize(num = n()) %>%
+  arrange(-desc(condition))
+
+tree_by_tree %>% 
+  group_by(space_type) %>%
+  dplyr::summarize(num = n()) %>%
+  arrange(desc(num))
+
+## Summary analyses 
+
+# Condition by neighborhood including "potential" tree sites
+tree_condition_by_nbr_all <- tree_by_tree %>% 
+  group_by(nbrdesc, condition) %>%
+  summarize(num = n()) %>%
+  mutate(perc = round(100*(num / sum(num)), 2)) %>%
+  select(-num) %>%
+  spread(condition, perc)
+
+# Condition by neighborhood not including "potential" tree sites
+tree_condition_by_nbr_filled <- tree_by_tree %>% 
+  #filter(!space_type %in% "potential") %>%
+  filter(!str_detect(space_type, "potential")) %>%
   group_by(nbrdesc, condition) %>%
   summarize(num = n()) %>%
   mutate(perc = round(100*(num / sum(num)), 2)) %>%
@@ -69,7 +91,9 @@ summary_tbl_1 <- tree_by_tree %>%
             avg_ht = round(mean(tree_ht), 2),
             combined_diam = sum(dbh),
             avg_diam = round(mean(dbh), 2)
-            ) %>%
+  ) %>%
   left_join(tree_condition_by_nbr)
 
 write_csv(summary_tbl_1, "output/data/tree_by_tree_neighborhood_summary.csv")
+
+
