e5b0cc6e66ff3fadb7fa3dded09db5132d5b7cde
Roxanne Ready <shardsofblue@gmail.com>
2019-07-29 18:39:28 -0400

    updates to canopy/poverty scatterplot

--- scripts/geography-based-analyses/poverty-to-canopy.R
+++ scripts/geography-based-analyses/poverty-to-canopy.R
@@ -46,6 +46,9 @@ csa_tree_temp_demographics <- read_csv(here(
 #                  y = `09-63_mean`,
 #                  color = `09-63_mean`),
 #              size=4) +
+#   # This section shows the trend line
+#   geom_smooth(aes(x = median_household_income/1000,
+#                   y = `09-63_mean`), se = FALSE) +
 #   coord_flip() +
 #   scale_color_viridis(direction = -1) +
 #   labs(title = "Median Income to Percent Tree Canopy",
@@ -53,14 +56,14 @@ csa_tree_temp_demographics <- read_csv(here(
 #        x = "",
 #        y = "") +
 #   scale_x_continuous(label = scales::dollar_format(),
-#                      breaks = seq(0, 150, 10)) + 
+#                      breaks = seq(0, 150, 10)) +
 #   scale_y_continuous(label = scales::percent_format(accuracy = 1.0),
-#                      breaks = seq(0, 1, .1)) + 
+#                      breaks = seq(0, 1, .1)) +
 #   theme_bw() +
 #   theme(legend.position = "none",
 #         plot.title = element_text(size = 20),
 #         plot.subtitle = element_text(size = 16))
-#   
+# 
 # # Save to file
 # ggsave(filename = "median-income-to-mean-tree-cover.png",
 #        device = "png", path = here("data", "output-data", "income-to-treecover"),
@@ -68,49 +71,43 @@ csa_tree_temp_demographics <- read_csv(here(
 
 
 # CSAs to call out
-callout_df <- c("Canton", "Greater Roland Park/Poplar Hill", "Greenmount East", "Patterson Park North & East")
+callout_ls <- c("Canton", "Clifton-Berea", "Greater Roland Park/Poplar Hill", "Greenmount East")
 
 ## Revised graph
 csa_tree_temp_demographics %>%
-  ggplot() +
-  # This section for the basic plot
-  geom_point(aes(x = percent_of_family_households_living_below_the_poverty_line/100,
-                 y = `09-63_mean`,
-                 color = `09-63_mean`),
+  # Start ggplot and set x and y for entire plot
+  ggplot(aes(
+    x = percent_of_family_households_living_below_the_poverty_line/100, 
+    y = `09-63_mean`
+    )) +
+  # This section for the basic scatterplot
+  geom_point(aes(color = `09-63_mean`),
              size=4) +
+  # This section shows the trend line
+  geom_smooth(se = FALSE, # Removes gray banding
+              #method = loess, method.args = list(family = "symmetric"), # Reduces outlier effect
+              color = "black"
+  ) +
   # This section for circling all sample neighborhood points
   geom_point(data = csa_tree_temp_demographics %>%
-               filter(csa2010 %in% c("Canton", "Greater Roland Park/Poplar Hill", "Greenmount East", "Patterson Park North & East")),
-             aes(x = percent_of_family_households_living_below_the_poverty_line/100,
-                 y = `09-63_mean`,
-                 color = `09-63_mean`),
+               filter(csa2010 %in% callout_ls),
+             aes(color = `09-63_mean`),
              size=6, shape = 1) +
   # This section for labeling Canton, etc.
   ggrepel::geom_label_repel(data = subset(csa_tree_temp_demographics, 
-                        csa2010 %in% c("Canton", "Greater Roland Park/Poplar Hill", "Greenmount East")),
-            aes(x = percent_of_family_households_living_below_the_poverty_line/100, 
-                y = `09-63_mean`, 
-                label = csa2010),
+                        csa2010 %in% callout_ls),
+            aes(label = csa2010),
             min.segment.length = .1,
             segment.alpha = .5,
             nudge_x = .05,
             nudge_y = .05) +
-  # This section for labeling Patterson Park (so it can be nudged)
-  ggrepel::geom_label_repel(data = subset(csa_tree_temp_demographics, 
-                        csa2010 %in% "Patterson Park North & East"),
-            aes(x = percent_of_family_households_living_below_the_poverty_line/100, 
-                y = `09-63_mean`, 
-                label = csa2010),
-            min.segment.length = .1,
-            segment.alpha = .5,
-            nudge_x = -.05) +
   # Colors and label formatting follow
-  coord_flip() +
+  #coord_flip() +
   scale_colour_gradient(low = "#E0FEA9", high = "#144A11") +
   labs(title = "Poverty to Tree Canopy",
-       subtitle = "Percent of households living below the poverty line \ncompared to the percent of tree cover in that neighborhood",
-       x = "",
-       y = "Tree Cover") +
+       subtitle = "Percent of households living below the poverty line \ncompared to the percent of tree cover",
+       x = "Percent of households living below the poverty line",
+       y = "") +
   scale_x_continuous(label = scales::percent_format(accuracy = 1.0),
                      breaks = seq(0, 1, .1)) + 
   scale_y_continuous(label = scales::percent_format(accuracy = 1.0),
@@ -121,7 +118,7 @@ csa_tree_temp_demographics %>%
         plot.subtitle = element_text(size = 12))
 
 # Save to file
-ggsave(filename = "poverty-to-mean-tree-cover.png",
+ggsave(filename = "poverty-to-mean-tree-cover2.png",
        device = "png", path = here("data", "output-data", "income-to-treecover"),
        width = 6, height = 6, units = "in")
 
