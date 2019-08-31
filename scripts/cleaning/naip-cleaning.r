2b3feaf56814701659eb7ae3e7eaacac5263c9f6
Sean Mussenden <smussenden@gmail.com>
2019-08-31 11:09:07 -0400

    adding input data

--- scripts/cleaning/naip-cleaning.r
+++
@@ -1,5 +0,0 @@
-install.packages('rgdal')
-library(rgdal)
-
-#### Load NAIP data #### 
-naip_2018_baltimore <- readGDAL("data/input-data/naip/ortho_1-1_hc_s_md510_2018_1/ortho_1-1_hc_s_md510_2018_1.sid")
